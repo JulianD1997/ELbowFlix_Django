5aa5863f87e27d220e35c1fbe8377c6df24e1d60
Julian Casallas <julian.casallasb@gmail.com>
2023-04-13 21:17:46 -0500

    libraries used in this project

--- ElbowFlix/urls.py
+++ ElbowFlix/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, include
 
 from .views import get_video, index, movies_json
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index, name="name"),
+    path("", index, name="index"),
     path("movie/", movies_json, name="movies_json"),
     path("movie/<int:id>/", get_video, name="get_video"),
+    path('user/',include('users.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from .forms import UserForm
 
 # Create your views here.
+
+def register(request):
+    form = UserForm()
+    context={'form':form}
+    return render(request,'users/register.html',context)
